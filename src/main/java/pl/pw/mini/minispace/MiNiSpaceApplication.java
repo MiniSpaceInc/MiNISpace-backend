6ff2d9c247603d725e74da32718011a6bd497c1b
McK <maciej.mpluta@gmail.com>
2024-03-07 18:33:46 +0100

    Initial java app

---
+++ src/main/java/pl/pw/mini/minispace/MiNiSpaceApplication.java
@@ -0,0 +1,13 @@
+package pl.pw.mini.minispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiNiSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiNiSpaceApplication.class, args);
+    }
+
+}
