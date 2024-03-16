95a6addd691f2fbac8149bbcbdf8e96a4db5c524
PioRow <piotr.rowicki02@gmail.com>
2024-03-16 02:02:33 +0100

    EventDto/Event added

---
+++ src/main/java/pl/pw/mini/minispace/dtos/EventDto.java
@@ -0,0 +1,22 @@
+package pl.pw.mini.minispace.dtos;
+import  pl.pw.mini.minispace.entities.Event;
+
+import java.time.LocalDateTime;
+
+public record EventDto(int id, String uud, String name, String Organizer, LocalDateTime date,String description) {
+    public static EventDto valueFrom(Event e)
+    {
+        return new EventDto(e.getId(), e.getUuid(),e.getName(),e.getOrganizer(),e.getDate(),e.getDescription());
+    }
+    public static Event ConvertToEvent(EventDto eDto)
+    {
+        Event e=new Event();
+        e.setId(eDto.id);
+        e.setDate(eDto.date);
+        e.setUuid(eDto.uud);
+        e.setOrganizer(eDto.Organizer);
+        e.setName(eDto.name);
+        e.setDescription(eDto.description);
+        return e;
+    }
+}
--- src/main/java/pl/pw/mini/minispace/entities/Event.java
+++ src/main/java/pl/pw/mini/minispace/entities/Event.java
@@ -1,11 +1,28 @@
 package pl.pw.mini.minispace.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
 @Entity
 @Table
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column
+    private String uuid;
+    @Column
+    private String name;
+    @Column
+    private String organizer;
+    @Column
+    private LocalDateTime date;
+    @Column
+        private String description;
+
 }
