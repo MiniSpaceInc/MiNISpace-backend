8e97c0ada2979fb67d381e4e6a065b4db24019d2
Adrian Rudź <01171340@pw.edu.pl>
2024-03-23 13:12:20 +0100

    Refactored PostDto

--- src/main/java/pl/pw/mini/minispace/dtos/PostDto.java
+++ src/main/java/pl/pw/mini/minispace/dtos/PostDto.java
@@ -2,5 +2,9 @@ package pl.pw.mini.minispace.dtos;
 
 import java.time.LocalDateTime;
 
-public record PostDto(Long id, String content, LocalDateTime datePosted) {
+public class PostDto {
+    private Long id;
+    private Long eventId;
+    private String content;
+    private LocalDateTime datePosted;
 }
