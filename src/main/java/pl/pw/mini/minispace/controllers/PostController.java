4f6434406f0f18dac5df2ec32b80aef412ee58f0
Adrian Rudź <01171340@pw.edu.pl>
2024-03-23 15:37:29 +0100

    Added PostController with functionalities: - adding new post - finding post by id

---
+++ src/main/java/pl/pw/mini/minispace/controllers/PostController.java
@@ -0,0 +1,25 @@
+package pl.pw.mini.minispace.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.pw.mini.minispace.dtos.PostDto;
+import pl.pw.mini.minispace.services.post.PostServiceFacade;
+
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+@RestController
+public class PostController {
+
+    private final PostServiceFacade postServiceFacade;
+
+    @PostMapping("/add")
+    public ResponseEntity<PostDto> addNewPost(@RequestBody PostDto postDto) {
+        return ResponseEntity.ok(postServiceFacade.addPost(postDto));
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostDto> findPost(@PathVariable Long postId) {
+        return ResponseEntity.ok(postServiceFacade.findById(postId));
+    }
+}
