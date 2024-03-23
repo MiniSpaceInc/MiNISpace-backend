279f0b164f1597391d8c443537c6bc610069e76f
Adrian Rudź <01171340@pw.edu.pl>
2024-03-23 15:06:17 +0100

    Added implementation for PostValidator

--- src/main/java/pl/pw/mini/minispace/validators/PostValidatorImpl.java
+++ src/main/java/pl/pw/mini/minispace/validators/PostValidatorImpl.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.pw.mini.minispace.entities.Post;
+import pl.pw.mini.minispace.enums.MiniSpaceMessages;
+import pl.pw.mini.minispace.exceptions.ValidationException;
+
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -11,6 +15,13 @@ import pl.pw.mini.minispace.entities.Post;
 public class PostValidatorImpl implements PostValidator {
     @Override
     public void validateNewPost(Post post) {
-        throw new UnsupportedOperationException();
+        if (Objects.isNull(post.getContent())) {
+            throw new ValidationException(String.format(MiniSpaceMessages.FIELD_IS_NULL_MESSAGE.getMessage(), "content"));
+        }
+
+        if (post.getContent().isEmpty()) {
+            throw new ValidationException(String.format(MiniSpaceMessages.FIELD_IS_EMPTY_MESSAGE.getMessage(), "content"));
+        }
     }
+
 }
