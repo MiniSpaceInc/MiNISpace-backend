7be1b52f61939d22d000bcb9968b8acf7e6c6448
Adrian Rudź <01171340@pw.edu.pl>
2024-03-23 16:04:51 +0100

    Added implementation for DeletePostService deletePost method

--- src/main/java/pl/pw/mini/minispace/services/post/DeletePostServiceImpl.java
+++ src/main/java/pl/pw/mini/minispace/services/post/DeletePostServiceImpl.java
@@ -1,8 +1,11 @@
 package pl.pw.mini.minispace.services.post;
 
 import lombok.AllArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import pl.pw.mini.minispace.daos.PostRepository;
+import pl.pw.mini.minispace.enums.MiniSpaceMessages;
+import pl.pw.mini.minispace.exceptions.EntityNotFoundException;
 
 @AllArgsConstructor
 @Service
@@ -12,6 +15,11 @@ public class DeletePostServiceImpl implements DeletePostService {
 
     @Override
     public void deletePost(Long id) {
-        throw new UnsupportedOperationException();
+        if (postRepository.findById(id).isPresent()) {
+            postRepository.deleteById(id);
+        }
+        else {
+            throw new EntityNotFoundException(String.format(MiniSpaceMessages.ENTITY_NOT_FOUND_MESSAGE.getMessage(), "Post", id));
+        }
     }
 }
