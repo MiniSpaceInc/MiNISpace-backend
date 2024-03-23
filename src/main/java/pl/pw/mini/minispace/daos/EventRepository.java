a05e4b696acdc11af5a6a046c3bfaf9b8f1821bf
Adrian Rudź <01171340@pw.edu.pl>
2024-03-23 13:49:49 +0100

    Refactored EventServiceRepository and added findById method in EventService

--- src/main/java/pl/pw/mini/minispace/daos/EventRepository.java
+++ src/main/java/pl/pw/mini/minispace/daos/EventRepository.java
@@ -3,6 +3,6 @@ package pl.pw.mini.minispace.daos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.pw.mini.minispace.entities.Event;
 
-public interface EventRepository extends JpaRepository<Event,Integer> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
