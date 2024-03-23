a0f45ae0c7b792bdbe15c44216598d5e54c02e26
Adrian Rudź <01171340@pw.edu.pl>
2024-03-23 13:13:21 +0100

    Added specific exceptions

---
+++ src/main/java/pl/pw/mini/minispace/exceptions/ValidationException.java
@@ -0,0 +1,11 @@
+package pl.pw.mini.minispace.exceptions;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
