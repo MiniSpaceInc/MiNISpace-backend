b19fa0ede18c9161205f2e3fcf2b56bc0a2b45c1
Adrian Rudź <01171340@pw.edu.pl>
2024-03-24 13:31:58 +0100

    Added definition of searchingPosts

--- src/main/java/pl/pw/mini/minispace/services/post/SearchPostService.java
+++ src/main/java/pl/pw/mini/minispace/services/post/SearchPostService.java
@@ -1,7 +1,10 @@
 package pl.pw.mini.minispace.services.post;
 
+import org.springframework.data.domain.Page;
+import pl.pw.mini.minispace.dtos.post.PostSearchDetailsDto;
 import pl.pw.mini.minispace.entities.Post;
 
 public interface SearchPostService {
     Post findById(Long id);
+    Page<Post> searchPosts(PostSearchDetailsDto postSearchDetailsDto);
 }
--- src/main/java/pl/pw/mini/minispace/services/post/SearchPostServiceImpl.java
+++ src/main/java/pl/pw/mini/minispace/services/post/SearchPostServiceImpl.java
@@ -2,8 +2,10 @@ package pl.pw.mini.minispace.services.post;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import pl.pw.mini.minispace.daos.PostRepository;
+import pl.pw.mini.minispace.dtos.post.PostSearchDetailsDto;
 import pl.pw.mini.minispace.entities.Post;
 import pl.pw.mini.minispace.enums.MiniSpaceMessages;
 import pl.pw.mini.minispace.exceptions.EntityNotFoundException;
@@ -19,5 +21,10 @@ public class SearchPostServiceImpl implements SearchPostService {
     public Post findById(Long id) {
         return postRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(String.format(MiniSpaceMessages.ENTITY_NOT_FOUND_MESSAGE.getMessage(), "Post", id)));
+    }
+
+    @Override
+    public Page<Post> searchPosts(PostSearchDetailsDto postSearchDetailsDto) {
+        throw new UnsupportedOperationException();
     }
 }
